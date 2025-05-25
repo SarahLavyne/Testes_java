498785a4f524959a689877da42518c867ad65053
SarahLavyne <sarahlavynem@gmail.com>
2025-05-25 17:01:39 -0300

    Update Main.java

--- Teste_banco/Main.java
+++ Teste_banco/Main.java
@@ -5,9 +5,9 @@ public class Main {
 		Banco novaConta = new Banco("Sarah", 1, 1); // Cria os dados no objeto
 	
 		
-		novaConta.depositar(100.0); // Aciona a função depositar
-		novaConta.mostrar(); // acima  a função mostrar
-		novaConta.sacar(20.5); //acima a função sacar
+		novaConta.depositar(100.0); // Chama a função depositar
+		novaConta.mostrar(); // Chama  a função mostrar
+		novaConta.sacar(20.5); //Chama a função sacar
 		novaConta.mostrar();
 		
 	}
